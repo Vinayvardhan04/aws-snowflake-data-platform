b1c0d701bbd2d4375e6c2fe35e7e319edbd6952a
Vinay Vardhan Poldasu <vinayvardhan0402@gmail.com>
2026-01-05 20:30:25 -0600

    Add S3 access policies for Lambda and Glue

---
+++ terraform/terraform/policies.tf
@@ -0,0 +1,38 @@
+# ---------------------------
+# S3 Access Policy
+# ---------------------------
+resource "aws_iam_policy" "s3_access_policy" {
+  name = "de-s3-access-${var.environment}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.bronze.arn,
+          "${aws_s3_bucket.bronze.arn}/*",
+          aws_s3_bucket.silver.arn,
+          "${aws_s3_bucket.silver.arn}/*",
+          aws_s3_bucket.artifacts.arn,
+          "${aws_s3_bucket.artifacts.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3_access" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
